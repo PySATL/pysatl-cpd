dd751e062c95628419be36b9c9012f89c1cd0ce4
Lesh79 <Temerlan.axmetov.75@mail.ru>
2024-11-06 22:52:05 +0300

    Feat: added probability calculation from triangles

--- CPDShell/Core/algorithms/GpraphCPD/graph_cpd.py
+++ CPDShell/Core/algorithms/GpraphCPD/graph_cpd.py
@@ -28,6 +28,32 @@ class GraphCPD(IGraphCPD):
         zg = -((self.graph.check_edges_exist(thao) - self.calculation_e(thao)) / math.sqrt(self.calculation_var(thao)))
         return zg
 
+    def calculation_probability(self, lower_order: int, higher_order: int) -> float:
+        n = higher_order - lower_order
+        combinations = (n * (n - 1) * (n - 2)) / 6
+        p = (self.graph.sum_triangle(lower_order, higher_order) / combinations) ** (1 / 3)
+        return p
+
+    def probability_diff(self):
+        change_point = []
+        probability = None
+        threshold = 0.25
+        window_zise = 20
+
+        for thao in range(0, self.size, window_zise):
+            current_probability = self.calculation_probability(thao, thao + window_zise)
+            print(f"thao = {thao}, current_probability = {current_probability}, previous_probability = {probability}")
+
+            if probability is None and current_probability != 0.0:
+                probability = current_probability
+            elif abs(current_probability - probability) > threshold:
+                change_point.append(thao)
+                probability = current_probability
+            else:
+                probability = current_probability
+
+        return change_point
+
     def find_changepoint(self, border: float) -> list[int]:
         change_point_list: list[int] = []
         for t in range(1, self.size):
--- CPDShell/main.py
+++ CPDShell/main.py
@@ -1,89 +1,30 @@
 from pathlib import Path
 
-from CPDShell.Core.algorithms.bayesian_algorithm import BayesianAlgorithm
-from CPDShell.Core.algorithms.BayesianCPD.detectors.drop_detector import DropDetector
-from CPDShell.Core.algorithms.BayesianCPD.detectors.simple_detector import SimpleDetector
-from CPDShell.Core.algorithms.BayesianCPD.hazards.constant_hazard import ConstantHazard
-from CPDShell.Core.algorithms.BayesianCPD.likelihoods.gaussian_unknown_mean_and_variance import (
-    GaussianUnknownMeanAndVariance,
-)
-from CPDShell.Core.algorithms.BayesianCPD.localizers.simple_localizer import SimpleLocalizer
-from CPDShell.Core.algorithms.knn_algorithm import KNNAlgorithm
+from CPDShell.Core.algorithms.GpraphCPD.Builders.matrix_builder import AdjacencyMatrixBuilder
+from CPDShell.Core.algorithms.GpraphCPD.graph_cpd import GraphCPD
 from CPDShell.generator.generator import ScipyDatasetGenerator
 from CPDShell.generator.saver import DatasetSaver
-from CPDShell.shell import CPDShell
+
+# adj_matrix = np.array([
+#     [0, 1, 1, 0],
+#     [1, 0, 1, 1],
+#     [1, 1, 0, 1],
+#     [0, 1, 1, 0]
+# ])
+# graph = GraphMatrix(adj_matrix, 5)
+# print(graph.sum_triangle())
 
 path_string = "tests/test_CPDShell/test_configs/test_config_exp.yml"
-distributions_name = "exp"
+distributions_name = "triangle"
 
 saver = DatasetSaver(Path(), True)
 generated = ScipyDatasetGenerator().generate_datasets(Path(path_string), saver)
 data, expected_change_points = generated[distributions_name]
-
-print("Expected change points:", expected_change_points)
-
-# Graph algorithm demo
-graph_cpd = CPDShell(data)
-graph_cpd.scrubber.window_length = 150
-graph_cpd.scrubber.movement_k = 2.0 / 3.0
-
-res_graph = graph_cpd.run_cpd()
-res_graph.visualize(True)
-print("Graph algorithm")
-print(res_graph)
-
-
-# k-NN based algorithm demo
-def metric(obs1: float, obs2: float) -> float:
-    return abs(obs1 - obs2)
-
-
-K = 5
-KNN_THRESHOLD = 3.5
-
-knn_algorithm = KNNAlgorithm(metric, K, KNN_THRESHOLD)
-knn_cpd = CPDShell(data, knn_algorithm)
-
-knn_cpd.scrubber.window_length = 16
-knn_cpd.scrubber.movement_k = 0.5
-knn_cpd.scenario.change_point_number = 4
-
-res_knn = knn_cpd.run_cpd()
-res_knn.visualize(True)
-print("k-NN based algorithm")
-print(res_knn)
-
-
-# Bayesian algorithm demo
-BAYESIAN_THRESHOLD = 0.1
-NUM_OF_SAMPLES = 1000
-SAMPLE_SIZE = 500
-BERNOULLI_PROB = 1.0 - 0.5 ** (1.0 / SAMPLE_SIZE)
-HAZARD_RATE = 1 / BERNOULLI_PROB
-LEARNING_SAMPLE_SIZE = 50
-BAYESIAN_DROP_THRESHOLD = 0.7
-
-constant_hazard = ConstantHazard(HAZARD_RATE)
-gaussian_likelihood = GaussianUnknownMeanAndVariance()
-
-simple_detector = SimpleDetector(BAYESIAN_THRESHOLD)
-drop_detector = DropDetector(BAYESIAN_DROP_THRESHOLD)
-
-simple_localizer = SimpleLocalizer()
-
-bayesian_algorithm = BayesianAlgorithm(
-    learning_steps=LEARNING_SAMPLE_SIZE,
-    likelihood=gaussian_likelihood,
-    hazard=constant_hazard,
-    detector=simple_detector,
-    localizer=simple_localizer,
-)
-
-bayesian_cpd = CPDShell(data, bayesian_algorithm)
-bayesian_cpd.scrubber.window_length = 500
-bayesian_cpd.scrubber.movement_k = 2.0 / 3.0
-
-res_bayes = bayesian_cpd.run_cpd()
-res_bayes.visualize(True)
-print("Bayesian algorithm")
-print(res_bayes)
+arg = 0.5
+builder = AdjacencyMatrixBuilder(data, lambda a, b: abs(a - b) <= arg)
+graph = builder.build_graph()
+cpd = GraphCPD(graph)
+# print(graph.sum_triangle())
+# print(cpd.calculation_probability())
+print(cpd.probability_diff())
+# print(cpd.scrubber(10))
