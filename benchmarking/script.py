6f0bf2bd17ad56def2c046e7a489ac15ec9cc6e6
Alexey <alexdtat@gmail.com>
2025-05-18 19:33:21 +0300

    feat(bayesian online algorith benchmark): add results postprocessing

--- benchmarking/script.py
+++ benchmarking/script.py
@@ -1,5 +1,7 @@
+import ast
 from pathlib import Path
 
+import numpy as np
 import pandas as pd
 
 from benchmarking.worker import Worker
@@ -117,4 +119,126 @@ def run_benchmark():
     )
 
 
-run_benchmark()
+def clean_data():
+    df = pd.read_csv("experiment_results.csv")
+
+    cleaned_df = df.drop_duplicates(
+        subset=["configuration", "experiment_number", "threshold", "learning_sample_size"], keep="first"
+    ).reset_index(drop=True)
+
+    cleaned_df.to_csv("cleaned_experiment_results.csv", index=False)
+
+
+def evaluate_row(row):
+    true_cp = row["change_point"]
+    detected_cps = row["detected_change_points"]
+    detection_delays = row["detection_delays"]
+
+    tp = 0
+    has_fp = False
+    fn = 0
+
+    delays = np.array(detection_delays)
+    cps = np.array(detected_cps)
+    delay_mean = delays.mean()
+    labeled_delay_mean = None
+
+    if pd.notnull(true_cp):
+        labeled_delay_mean = (cps + delays - true_cp).mean()
+
+        window_start = true_cp - 25
+        window_end = true_cp + 25
+
+        in_window = [cp for cp in detected_cps if window_start <= cp <= window_end]
+
+        if in_window:
+            tp = 1
+            has_fp = (
+                len([cp for cp in detected_cps if not (window_start <= cp <= window_end)]) > 0 or len(in_window) > 1
+            )
+        else:
+            fn = 1
+            has_fp = len(detected_cps) > 0
+    else:
+        has_fp = len(detected_cps) > 0
+
+    return pd.Series(
+        {
+            "TP": tp,
+            "FP": 1 if has_fp else 0,
+            "FN": fn,
+            "delay": np.float64(delay_mean),
+            "labeled_delay": labeled_delay_mean,
+        }
+    )
+
+
+def evaluate_confusion_matrices():
+    df = pd.read_csv("cleaned_experiment_results.csv")
+    df["detected_change_points"] = df["detected_change_points"].apply(ast.literal_eval)
+    df["detection_delays"] = df["detection_delays"].apply(ast.literal_eval)
+
+    metrics = df.apply(evaluate_row, axis=1)
+    result = pd.concat([df[["configuration", "threshold", "learning_sample_size"]], metrics], axis=1)
+
+    final_metrics = (
+        result.groupby(["configuration", "threshold", "learning_sample_size"])
+        .agg(
+            {
+                "TP": "sum",
+                "FP": "sum",
+                "FN": "sum",
+                "delay": ["mean", "std", "median"],
+                "labeled_delay": ["mean", "std", "median"],
+            }
+        )
+        .reset_index()
+    )
+
+    final_metrics["power"] = final_metrics["TP"] / 1000
+    final_metrics["significance"] = final_metrics["FP"] / 1000
+
+    confusion_values = ["TP", "FP", "FN"]
+    final_metrics[confusion_values] = final_metrics[confusion_values].astype(int)
+
+    final_metrics.columns = [
+        "configuration",
+        "threshold",
+        "learning_sample_size",
+        "TP",
+        "FP",
+        "FN",
+        "delay_mean",
+        "delay_std",
+        "delay_median",
+        "labeled_delay_mean",
+        "labeled_delay_std",
+        "labeled_delay_median",
+        "power",
+        "significance",
+    ]
+
+    to_round = [
+        "power",
+        "significance",
+        "delay_mean",
+        "delay_std",
+        "delay_median",
+        "labeled_delay_mean",
+        "labeled_delay_std",
+        "labeled_delay_median",
+    ]
+    final_metrics[to_round] = final_metrics[to_round].round(3)
+
+    final_metrics.to_csv(
+        "D:\\Alexey\\PyCharmProjects\\PySATL-CPD-Module\\benchmarking\\cleaned_confusion_matrices.csv", index=False
+    )
+
+
+# evaluate_confusion_matrices()
+df = pd.read_csv("cleaned_confusion_matrices.csv")
+drop_configs = ["normal", "exponential", "beta", "weibull", "uniform", "beta-beta", "weibull-uniform"]
+filtered_df = df.loc[~df["configuration"].isin(drop_configs)]
+print(filtered_df.loc[filtered_df["delay_mean"].idxmax()])
+print(filtered_df.loc[filtered_df["power"].idxmin()])
+# print(filtered_df[['configuration', 'threshold', 'learning_sample_size', 'power']].to_string())
